44f4ecce054e5413763133318221f736da275b4f
pkorsback <peter.korsback@creuna.se>
2017-11-27 13:23:26 +0100

    Add first abstraction

---
+++ src/Howff.Episerver.Abstractions/ISiteDefinition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using EPiServer.Core;
+using EPiServer.Web;
+
+namespace Howff.Episerver.Abstractions {
+	public interface ISiteDefinition {
+		ContentReference ContentAssetsRoot { get; }
+		SiteDefinition CreateWritableClone();
+		SiteDefinition Current { get; set; }
+		SiteDefinition Empty { get; }
+		ContentReference GlobalAssetsRoot { get; }
+		IList<HostDefinition> Hosts { get; set; }
+		Guid Id { get; set; }
+		bool IsReadOnly { get; }
+		object this[string key] { get; set; }
+		string Name { get; set; }
+		ContentReference RootPage { get; }
+		ContentReference SiteAssetsRoot { get; set; }
+		Uri SiteUrl { get; set; }
+		ContentReference StartPage { get; set; }
+		ContentReference WasteBasket { get; }
+	}
+}
---
+++ src/Howff.Episerver.Abstractions/SiteDefinitionAdapter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+using EPiServer.Core;
+using EPiServer.Web;
+
+namespace Howff.Episerver.Abstractions {
+	public class SiteDefinitionAdapter : ISiteDefinition {
+		public ContentReference ContentAssetsRoot => SiteDefinition.Current.ContentAssetsRoot;
+
+		public SiteDefinition Empty => SiteDefinition.Empty;
+
+		public virtual SiteDefinition CreateWritableClone() {
+			return Current.CreateWritableClone();
+		}
+
+		public SiteDefinition Current {
+			get => SiteDefinition.Current;
+			set => SiteDefinition.Current = value;
+		}
+
+		public ContentReference GlobalAssetsRoot => SiteDefinition.Current.GlobalAssetsRoot;
+
+		public IList<HostDefinition> Hosts {
+			get => SiteDefinition.Current.Hosts;
+			set => SiteDefinition.Current.Hosts = value;
+		}
+
+		public Guid Id {
+			get => SiteDefinition.Current.Id;
+			set => SiteDefinition.Current.Id = value;
+		}
+
+		public bool IsReadOnly => SiteDefinition.Current.IsReadOnly;
+
+		public object this[string key] {
+			get => SiteDefinition.Current[key];
+			set => SiteDefinition.Current[key] = value;
+		}
+
+		public string Name {
+			get => SiteDefinition.Current.Name;
+			set => SiteDefinition.Current.Name = value;
+		}
+
+		public ContentReference RootPage => SiteDefinition.Current.RootPage;
+
+		public ContentReference SiteAssetsRoot {
+			get => SiteDefinition.Current.SiteAssetsRoot;
+			set => SiteDefinition.Current.SiteAssetsRoot = value;
+		}
+
+		public Uri SiteUrl {
+			get => SiteDefinition.Current.SiteUrl;
+			set => SiteDefinition.Current.SiteUrl = value;
+		}
+
+		public ContentReference StartPage {
+			get => SiteDefinition.Current.StartPage;
+			set => SiteDefinition.Current.StartPage = value;
+		}
+
+		public ContentReference WasteBasket => SiteDefinition.Current.WasteBasket;
+	}
+}
