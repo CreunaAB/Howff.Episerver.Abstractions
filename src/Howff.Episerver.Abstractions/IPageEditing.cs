2abef3b1422d7486e55c43dacabdf9c4d4227630
pkorsback <peter.korsback@creuna.se>
2017-11-27 13:53:12 +0100

    Add PageEditing abstraction and adapter

---
+++ src/Howff.Episerver.Abstractions/IPageEditing.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+
+using EPiServer.Core;
+
+namespace Howff.Episerver.Abstractions {
+	public interface IPageEditing {
+		bool PageIsInEditMode { get; }
+
+		string GetEditUrl(ContentReference contentLink);
+		string GetEditUrlForLanguage(ContentReference contentLink, string language);
+		string GetChannel(HttpContextBase httpContext);
+	}
+}
---
+++ src/Howff.Episerver.Abstractions/PageEditingAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+
+using EPiServer.Core;
+using EPiServer.Editor;
+
+namespace Howff.Episerver.Abstractions {
+	public class PageEditingAdapter : IPageEditing {
+		public bool PageIsInEditMode => PageEditing.PageIsInEditMode;
+
+		public string GetEditUrl(ContentReference contentLink) {
+			return PageEditing.GetEditUrl(contentLink);
+		}
+
+		public string GetEditUrlForLanguage(ContentReference contentLink, string language) {
+			return PageEditing.GetEditUrlForLanguage(contentLink, language);
+		}
+
+		public string GetChannel(HttpContextBase httpContext) {
+			return PageEditing.GetChannel(httpContext);
+		}
+	}
+}
